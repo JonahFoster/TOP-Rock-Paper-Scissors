13e16f8f54e01b1ffc17a0c3d6d9f905161663c9
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-05-08 17:23:10 -0400

    Computer choice implemented

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let words = ["rock", "paper", "scissors"];
+    let n = Math.floor(Math.random() * words.length);
+
+    return words[n];
+
+}
+
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+
+console.log(getComputerChoice("words"))
+
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+
+console.log(getComputerChoice("words"))
+
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+
+console.log(getComputerChoice("words"))
+
+
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+console.log(getComputerChoice("words"))
+
