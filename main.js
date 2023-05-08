aba3c3f8dcceb08ce77166dab2599cc2da77e5e5
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-05-08 17:57:00 -0400

    playRound implemented

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-console.log("Hello World")
-
 function getComputerChoice() {
+    // Define words variable
     let words = ["rock", "paper", "scissors"];
+    // Randomly select a word
     let n = Math.floor(Math.random() * words.length);
 
     return words[n];
 
 }
 
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "Computer Wins! Paper beats Rock."
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Player Wins! Rock beats Scisors."
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Computer Wins! Scissors beats Paper."
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Player Wins! Paper beats Rock."
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Computer Wins! Scissors beats Rock."
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Player Wins! Scissors beats Paper."
+    }
+}
 
-console.log(getComputerChoice("words"))
-
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-
-console.log(getComputerChoice("words"))
-
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-
-console.log(getComputerChoice("words"))
-
-
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-console.log(getComputerChoice("words"))
-
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
